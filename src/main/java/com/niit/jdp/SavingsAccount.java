6d1489d6a8a21f4bcf14a66a44c620ad2c3756d8
Philip Meshach <philipmeshach.pm@gmail.com>
2022-08-23 18:04:17 +0530

    Final commit

--- src/main/java/com/niit/jdp/SavingsAccount.java
+++ src/main/java/com/niit/jdp/SavingsAccount.java
@@ -1,5 +1,14 @@
 package com.niit.jdp;
 
 public class SavingsAccount {
+    String accountNumber;
+    String accountHolderName;
+    String AccountBalance;
+    String accountStatus;
 
+
+    public void behaviour(String debitCash, String creditCash, String maintainTransfer) {
+
+        return;
+    }
 }
