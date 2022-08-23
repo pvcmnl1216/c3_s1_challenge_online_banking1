50143b60a62707903b86a64f27271215b8de78fb
Philip Meshach <philipmeshach.pm@gmail.com>
2022-08-23 17:03:49 +0530

    Create class for the customer is done

---
+++ src/main/java/com/niit/jdp/Customer.java
@@ -0,0 +1,13 @@
+package com.niit.jdp;
+
+public class Customer {
+    String name;
+    String address;
+    String contactNumber;
+    String age;
+
+    public void behaviour(String withdraw, String deposit, String checkBalance) {
+        return;
+    }
+
+}
